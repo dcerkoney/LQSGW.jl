b114e81219fe56c8348c23c96043c32ce5183b51
dcerkoney <dcerkoney@gmail.com>
2025-01-14 15:55:45 -0500

    WIP: Add function `vertex_matsubara_sum`

--- example/get_one_loop_Fs.jl
+++ example/get_one_loop_Fs.jl
@@ -4,6 +4,7 @@ using ElectronGas
 using ElectronLiquid
 using GreenFunc
 using JLD2
+using LinearAlgebra
 using LsqFit
 using Lehmann
 using LQSGW
@@ -16,6 +17,7 @@ using Roots
 @pyimport scienceplots  # for style "science"
 @pyimport scipy.interpolate as interp
 
+const iw0 = im * π / β
 const alpha_ueg = (4 / 9π)^(1 / 3)
 
 # Mapping of interaction types to Landau parameters
@@ -53,6 +55,77 @@ rcParams["font.size"] = 16
 rcParams["mathtext.fontset"] = "cm"
 rcParams["font.family"] = "Times New Roman"
 
+# Specify the type of momentum and frequency (index) grids explicitly to ensure type stability
+const MomGridType = CompositeGrids.CompositeG.Composite{
+    Float64,
+    CompositeGrids.SimpleG.Arbitrary{Float64,CompositeGrids.SimpleG.ClosedBound},
+    CompositeGrids.CompositeG.Composite{
+        Float64,
+        CompositeGrids.SimpleG.Log{Float64},
+        CompositeGrids.SimpleG.Uniform{Float64,CompositeGrids.SimpleG.ClosedBound},
+    },
+}
+const MGridType = CompositeGrids.SimpleG.Arbitrary{Int64,CompositeGrids.SimpleG.ClosedBound}
+const FreqGridType =
+    CompositeGrids.SimpleG.Arbitrary{Float64,CompositeGrids.SimpleG.ClosedBound}
+
+@with_kw mutable struct OneLoopParams
+    # UEG parameters
+    beta::Float64
+    rs::Float64
+    Fs::Float64 = -0.0
+    Fa::Float64 = -0.0
+
+    mass2::Float64 = 1e-6     # fictitious Yukawa screening λ
+    massratio::Float64 = 1.0  # mass ratio m*/m
+
+    dim::Int = 3
+    spin::Int = 2
+
+    basic::Parameter.Para = Parameter.rydbergUnit(1.0 / beta, rs, dim; Λs=mass2, spin=spin)
+
+    kF::Float64 = basic.kF
+    EF::Float64 = basic.EF
+    β::Float64 = basic.β
+    me::Float64 = basic.me
+    ϵ0::Float64 = basic.ϵ0
+    e0::Float64 = basic.e0
+    μ::Float64 = basic.μ
+    NF::Float64 = basic.NF
+    NFstar::Float64 = basic.NF * massratio
+    qTF::Float64 = basic.qTF
+
+    fs::Float64 = Fs / NF
+    fa::Float64 = Fa / NF
+
+    # Momentum grid parameters
+    maxK::Float64 = 6 * kF
+    maxQ::Float64 = 6 * kF
+    Q_CUTOFF::Float64 = 1e-10 * kF
+
+    # Use ~100 k-points for the momentum grids
+    # NOTE: EL.jl default is `Nk, order = 16, 16` (~700 k-points)
+    Nk, order = 7, 7
+    qgrid::MomGridType =
+        CompositeGrid.LogDensedGrid(:uniform, [0.0, maxQ], [0.0, 2 * kF], 7, 0.01 * kF, 7)
+
+    # Use a sparse DLR grid for the bosonic Matsubara summation (~30-50 iνₘ-points)
+    dlr::DLRGrid{Float64,:ph} =
+        DLRGrid(; Euv=10 * EF, β=β, rtol=1e-10, isFermi=false, symmetry=:ph)
+    mgrid::MGridType = SimpleG.Arbitrary{Int64}(dlr.n)
+    vmgrid::Vector{Float64} = SimpleG.Arbitrary{Float64}(dlr.ωn)
+    Mmax::Int64 = maximum(mgrid)
+
+    # Incoming momenta k1, k2 and incident scattering angle
+    kamp1::Float64 = basic.kF
+    kamp2::Float64 = basic.kF
+    θ12::Float64 = π
+
+    # R grid data is precomputed in an initialization step
+    initialized::Bool = false
+    R::Matrix{Float64} = Matrix{Float64}(undef, length(qgrid), length(mgrid))
+end
+
 function spline(x, y, e; xmin=0.0, xmax=x[end])
     # generate knots with spline without constraints
     w = 1.0 ./ e
@@ -62,35 +135,15 @@ function spline(x, y, e; xmin=0.0, xmax=x[end])
     return __x, yfit
 end
 
-# exchange interaction (Ws + Wa \sigma\sigma)_ex to a direct interaction Ws'+Wa' \sigma\sigma 
-# # exchange S/A interaction projected to the spin-symmetric and antisymmetric parts
-# # NOTE: since Wae = 0, this just divides Ws by 2...
-function exchange_to_direct(Wse, Wae)
-    Ws = (Wse + 3 * Wae) / 2
-    Wa = (Wse - Wae) / 2
-    return Ws, Wa
-end
-
 """
 Get the symmetric l=0 Fermi-liquid parameter F⁰ₛ via Corradini's fit
 to the DMC compressibility enhancement [doi: 10.1103/PhysRevB.57.14569].
 """
-@inline function get_Fs(param::Parameter.Para)
-    kappa0_over_kappa = Interaction.compressibility_enhancement(param)
+@inline function get_Fs(param::OneLoopParams)
+    kappa0_over_kappa = Interaction.compressibility_enhancement(param.basic)
     # NOTE: NEFT uses opposite sign convention for F!
     # -F⁰ₛ = 1 - κ₀/κ
     return 1.0 - kappa0_over_kappa
-end
-
-"""
-Get the antisymmetric l=0 Fermi-liquid parameter F⁰ₐ via  Corradini's fit
-to the DMC susceptibility enhancement [doi: 10.1103/PhysRevB.57.14569].
-"""
-@inline function get_Fa(param::Parameter.Para)
-    chi0_over_chi = Interaction.spin_susceptibility_enhancement(param)
-    # NOTE: NEFT uses opposite sign convention for F!
-    # -F⁰ₐ = 1 - χ₀/χ
-    return 1.0 - chi0_over_chi
 end
 
 function lindhard(x)
@@ -113,7 +166,8 @@ function integrand_F0p(x, rs_tilde, Fs=0.0)
     return -x * NF_times_Rp_ex
 end
 
-function plot_integrand_F0p(param::Parameter.Para)
+function plot_integrand_F0p(param::OneLoopParams)
+    @unpack beta, dim = param
     rslist = [0, 1, 5, 10, 20, Inf]
     colorlist = [
         cdict["grey"],
@@ -126,7 +180,7 @@ function plot_integrand_F0p(param::Parameter.Para)
     fig, ax = plt.subplots()
     x = np.linspace(0, 1, 1000)
     for (rs, color) in zip(rslist, colorlist)
-        param_this_rs = Parameter.rydbergUnit(1.0 / param.beta, rs, param.dim)
+        param_this_rs = Parameter.rydbergUnit(1.0 / beta, rs, dim)
         Fs_RPA = 0.0
         Fs = -get_Fs(param_this_rs)
         if rs > 0.25 && rs != Inf
@@ -153,7 +207,8 @@ function plot_integrand_F0p(param::Parameter.Para)
     plt.close("all")
 end
 
-function get_analytic_F0p(param, rslist; plot=false)
+function get_analytic_F0p(param::OneLoopParams, rslist; plot=false)
+    @unpack beta, dim = param
     F0p_RPA = []
     F0p_R = []
     Fslist = []
@@ -163,7 +218,7 @@ function get_analytic_F0p(param, rslist; plot=false)
     F0p_RPA_inf = CompositeGrids.Interp.integrate1D(y0_RPA_inf, xgrid)
     println("F⁺₀[W₀](∞) = $(F0p_RPA_inf)")
     for rs in rslist
-        param_this_rs = Parameter.rydbergUnit(1.0 / param.beta, rs, param.dim)
+        param_this_rs = Parameter.rydbergUnit(1.0 / beta, rs, dim)
         rstilde = rs * alpha_ueg / π
         Fs = -get_Fs(param_this_rs)
         Fs_sc = get_tree_level_self_consistent_Fs(param_this_rs)
@@ -208,7 +263,7 @@ end
 """
 Solve I0[F+] = F+ / 2 to obtain the tree-level self-consistent value for F⁰ₛ.
 """
-function get_tree_level_self_consistent_Fs(param::Parameter.Para)
+function get_tree_level_self_consistent_Fs(param::OneLoopParams)
     @unpack rs = param
     function I0_R(x, y)
         ts = CompositeGrid.LogDensedGrid(:gauss, [0.0, 1.0], [0.0, 1.0], 32, 1e-8, 32)
@@ -220,43 +275,11 @@ function get_tree_level_self_consistent_Fs(param::Parameter.Para)
     return F0p_sc
 end
 
-function Π0_static(param::Parameter.Para, qgrid::QGT) where {QGT<:AbstractGrid}
-    @unpack kF, NF = param
-    xgrid = qgrid.mesh[1].grid / (2 * kF)
-    return -param.NF * lindhard.(xgrid)
-end
-
-"""
-The non-interacting (renormalized) Green's function G₀.
-As a first approximation, we neglect the mass renormalization, setting m* = m.
-"""
-function G0(
-    param::Parameter.Para,
-    kgrid::KGT,
-    wgrid::WGT,
-) where {KGT<:AbstractGrid,WGT<:AbstractGrid}
-    @unpack me, β, μ, kF = param
-    g0 = GreenFunc.MeshArray(wgrid, kgrid; dtype=ComplexF64)
-    for ind in eachindex(g0)
-        iw, ik = ind[1], ind[2]
-        g0[ind] = 1.0 / (im * wgrid[iw] - kgrid[ik]^2 / (2 * me) + μ)
-    end
-    return g0
-end
-
-"""
-G₀(k, iω)
-"""
-function G0(param::Parameter.Para, k, w)
-    @unpack me, μ = param
-    return 1.0 / (im * w - k^2 / (2 * me) + μ)
-end
-
 """
 The one-loop (GW) self-energy Σ₁.
 """
 function Σ1(
-    param::Parameter.Para,
+    param::OneLoopParams,
     kgrid::KGT;
     Fs=-0.0,
     int_type=:rpa,
@@ -270,7 +293,7 @@ function Σ1(
     minK = 1e-6 * kF
     # Get the One-loop self-energy
     sigma = Sigma.G0W0(
-        param,
+        param.basic,
         kgrid;
         Euv=Euv,
         rtol=rtol,
@@ -287,20 +310,20 @@ end
 Leading-order (one-loop) correction to Z_F.
 """
 function Z1(
-    param::Parameter.Para,
+    param::OneLoopParams,
     kgrid::KGT;
     Fs=-0.0,
     int_type=:rpa,
 ) where {KGT<:AbstractGrid}
-    sigma1 = Σ1(param, kgrid; Fs=Fs, int_type=int_type)
+    sigma1 = Σ1(param.basic, kgrid; Fs=Fs, int_type=int_type)
     # Z_F using improved finite-temperature scaling
-    return zfactor_fermi(param, sigma1)
+    return zfactor_fermi(param.basic, sigma1)
 end
 
 """
 Tree-level estimate of F⁺₀.
 """
-function F1(param::Parameter.Para; int_type=:rpa)
+function F1(param::OneLoopParams; int_type=:rpa)
     @unpack rs, kF, EF, NF = param
     rstilde = rs * alpha_ueg / π
     xgrid = CompositeGrid.LogDensedGrid(:gauss, [0.0, 1.0], [0.0, 1.0], 32, 1e-8, 32)
@@ -314,51 +337,99 @@ function F1(param::Parameter.Para; int_type=:rpa)
     return F0p_tree_level
 end
 
-function R_data(param::Parameter.Para)
-    @unpack kF, EF, β = param
-    dlr = DLRGrid(Euv=10 * EF, β=β, rtol=1e-10, isFermi=false, symmetry=:ph)
-    ngrid = dlr.n
-    qgrid = CompositeGrid.LogDensedGrid(:uniform, [0.0, 6 * kF], [0.0, 2 * kF], 16, 0.01 * kF, 16)
-    # Evaluate R(q, iνₘ) on a sparse DLR grid
-    Nmax = maximum(ngrid)
-    Nq, Nw = length(qgrid), length(ngrid)
-    Rs = zeros(Float64, (Nq, Nw))
-    for (ni, n) in enumerate(ngrid)
+function Π0_static(param::OneLoopParams, qgrid::QGT) where {QGT<:AbstractGrid}
+    @unpack kF, NF = param
+    xgrid = qgrid.mesh[1].grid / (2 * kF)
+    return -NF * lindhard.(xgrid)
+end
+
+"""
+The non-interacting (renormalized) Green's function G₀.
+As a first approximation, we neglect the mass renormalization, setting m* = m.
+"""
+function G0_data(
+    param::OneLoopParams,
+    kgrid::KGT,
+    wgrid::WGT,
+) where {KGT<:AbstractGrid,WGT<:AbstractGrid}
+    @unpack me, β, μ, kF = param
+    g0 = GreenFunc.MeshArray(wgrid, kgrid; dtype=ComplexF64)
+    for ind in eachindex(g0)
+        iw, ik = ind[1], ind[2]
+        g0[ind] = 1.0 / (im * wgrid[iw] - kgrid[ik]^2 / (2 * me) + μ)
+    end
+    return g0
+end
+
+function R_data(param::OneLoopParams)
+    @unpack basic, qgrid, mgrid, Mmax, fs = param
+    Nq, Nw = length(qgrid), length(mgrid)
+    R = zeros(Float64, (Nq, Nw))
+    for (ni, n) in enumerate(mgrid)
         for (qi, q) in enumerate(qgrid)
-            invKOinstant = 1.0 / ElectronLiquid.KOinstant(q, para)
-            # Rs = (vq+f)Π0/(1-(vq+f)Π0)
-            Pi[qi, ni] = ElectronLiquid.polarKW(q, n, para)
-            Rs[qi, ni] = Pi[qi, ni] / (invKOinstant - Pi[qi, ni])
+            invKOinstant = 1.0 / UEG.KOinstant(q, basic)
+            # R = (vq + f) / (1 - (vq + f) Π0) - f
+            Pi[qi, ni] = UEG.polarKW(q, n, basic)
+            R[qi, ni] = 1 / (invKOinstant - Pi[qi, ni]) - fs
         end
     end
-    # upsample to full bosonic Matsubara grid with indices ranging from -N to N
-    Rs = matfreq2matfreq(dlr, Rs, collect((-Nmax):Nmax); axis=2)
-    return real.(Rs)  # R(q, iνₘ) = R(q, -iνₘ) ⟹ R is real
+    # upsample to full frequency grid with indices ranging from -N to N
+    R = matfreq2matfreq(dlr, R, collect((-Mmax):Mmax); axis=2)
+    return real.(R)  # R(q, iνₘ) = R(q, -iνₘ) ⟹ R is real
+end
+
+function initialize_R!(param::OneLoopParams)
+    param.R = R_data(param)
+    param.initialized = true
+end
+
+"""
+G₀(k, iωₙ)
+"""
+function G0(param::OneLoopParams, k, wn)
+    @unpack me, μ, β = param
+    return 1.0 / (im * wn - k^2 / (2 * me) + μ)
+end
+
+"""
+R(q, n) via multilinear interpolation, where n indexes bosonic Matsubara frequencies iνₙ.
+"""
+function R(param::OneLoopParams, q, n)
+    if q > param.maxQ
+        return 0.0
+    end
+    if q ≤ param.Q_CUTOFF
+        q = param.Q_CUTOFF
+    end
+    return UEG.linear2D(param.R, param.qgrid, param.mgrid, q, n)
+end
+
+function vertex_matsubara_sum(param::OneLoopParams, q, θq, φq)
+    @unpack kF, β, EF, kamp1, kamp2, θ12, mgrid, vmgrid, Mmax = param
+
+    # p1 = |k + q'|, p2 = |k' + q'|
+    vec_p1 = [0.0, 0.0, kamp1] + q * [sin(θq) * cos(φq), sin(θq) * sin(φq), cos(θq)]
+    vec_p2 = [kamp2, 0.0, 0.0] + q * [sin(θq) * cos(φq), sin(θq) * sin(φq), cos(θq)]
+    p1 = norm(vec_p1)
+    p2 = norm(vec_p2)
+
+    # S(iνₘ) = R(q', iν'ₘ) * g(p1, iω₀ + iν'ₘ) * g(p2, iω₀ + iν'ₘ)
+    s_ivm = Vector{ComplexF64}(undef, length(mgrid))
+    for (i, (m, vm)) in enumerate(zip(mgrid, vmgrid))
+        s_ivm[i] =
+            R(param, q, m) * G0(param, p1, iw0 + im * vm) * G0(param, p2, iw0 + im * vm)
+    end
+
+    # interpolate data for S(iνₘ) over entire frequency mesh from 0 to Mmax
+    s_ivm_full = Interp.interp1DGrid(s_ivm, mgrid, 0:Mmax)
+
+    # sum over iνₘ including negative frequency terms (S(iνₘ) = S(-iνₘ))
+    matsubara_sum = (s_ivm_full[0] + 2 * sum(s_ivm_full[2:end])) / β
+    return matsubara_sum
 end
 
 # 2RΛ₁
-function one_loop_vertex_corrections(
-    param::Parameter.Para;
-    kamp1=param.kF,
-    kamp2=param.kF,
-    θ12=π,
-)
-    @unpack kF, β, EF = param
-
-    function vertex_matsubara_sum(param::Parameter.Para, q, θq, φq)
-        s_ivm = []
-        # mlist = ... # sparse bosonic mesh {iνₘ} from DLR
-        for m in mlist
-            # A(iνₘ) = R(q', iν'ₘ) * g(k + q', iω₀ + iν'ₘ) * g(k' + q', iω₀ + iν'ₘ)
-            # gg = ...
-        end
-        # interpolate data for A(iνₘ) over entire frequency mesh from -M to M
-        mlist_full = collect((-M):M) * (im * 2π / β)
-        s_ivm_full = Interp.interp1DGrid(s_ivm, mlist, mlist_full)
-        # sum over iνₘ
-        matsubara_sum = s_ivm_full[0] + 2 * sum(s_ivm_full[2:end])
-        return matsubara_sum / β
-    end
+function one_loop_vertex_corrections(param::OneLoopParams)
 
     # integrate over loop momentum q
     qs = [kF]
@@ -373,6 +444,7 @@ function one_loop_vertex_corrections(
         end
     end
 
+    one_loop_vertex_corrections = missing
     return one_loop_vertex_corrections
 end
 
@@ -409,6 +481,20 @@ function main()
     beta = 40.0
     param = Parameter.rydbergUnit(1.0 / beta, rs, 3)
 
+    @unpack kF, EF, β = param
+
+    # Get coarse grid data for R(q, iνₘ)
+    Rs = R_data(param, qgrid, mgrid)
+
+    res, time = test_vertex_integral(
+        param,
+        #  args...
+    )
+
+    println("Result: $res")
+    println("Evaluation time: $time")
+    return
+
     # # l=0 analytic plots
     # rs_Fsm1 = 5.24881  # Fs(rs = 5.24881) ≈ -1 (using Perdew-Wang fit)
     # rslist = sort(unique([0.01; 0.025; 0.05; rs_Fsm1; collect(range(0.1, 10.0; step=0.1))]))
@@ -419,14 +505,6 @@ function main()
     # # Dimensionless angular momentum grid for k - k'
     # xgrid = CompositeGrid.LogDensedGrid(:gauss, [0.0, 1.0], [0.0, 1.0], 32, 1e-8, 32)
     # k_m_kps = @. 2 * kF * xgrid
-
-    res, time = test_vertex_integral(
-        param,
-        #  args...
-    )
-    println("Result: $res")
-    println("Evaluation time: $time")
-    return
 
     # # Use the tree-level self-consistent result as a starting point
     # # TODO: add a relative tolerance variable and a check for convergence
